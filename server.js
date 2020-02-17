87270b1f027c900d6ba1ae7ce5bc0dbcdfe602a6
Geizer6991 <geizer@ro.ru>
2020-02-17 15:22:37 +0400

    ti`s

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express       =   require('express');
 const cookieParser  =   require ('cookie-parser');
 const bodyParser    =   require('body-parser');
+const cors = require('cors')
+
+
 
 let app = express();
      app.use(cookieParser ());
      app.use(bodyParser.json());
      app.use(bodyParser.urlencoded({ extended: true }));
+     app.use(cors())
      app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
