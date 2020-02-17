7a63656910988c08754f983a184a072663deef5b
Geizer6991 <geizer@ro.ru>
2020-02-17 15:12:22 +0400

    ti`s

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ let app = express();
      app.use(cookieParser ());
      app.use(bodyParser.json());
      app.use(bodyParser.urlencoded({ extended: true }));
+     app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+     });
+
 
 let  login,UseR,z,f,s,x,password,user,randomNumber,UsER,post,options
     ,messageText,profile,idProf,userID,UserID,people
